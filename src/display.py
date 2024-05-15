cbdd03ce328ca453e5a3be29a2c80bb03edfe896
Alan Feder <alanfeder@gmail.com>
2024-05-14 20:50:53 -0400

    add footer

--- src/display.py
+++ src/display.py
@@ -85,6 +85,19 @@ def make_app(n_results: int) -> None:
     placeholder1 = 'e.g. What is the tidyverse?'
     chat_container = st.container()
     videos_container = st.container()
+    footer_container = st.container()
+
+    with footer_container:
+        st.divider()
+
+        st.caption('''This streamlit app was created for Alan Feder's [talk at the 10th Anniversary New York R Conference](https://rstats.ai/nyr.html).  The slides used are [here](https://bit.ly/nyr-rag).  The Github repository that houses all the code is [here](https://github.com/AlanFeder/nyr-rag-app) -- feel free to fork it and use it on your own!''')
+
+        st.divider()
+
+        st.subheader('Contact me!')
+        st.image('AJF_Headshot.jpg', width=60)
+        st.markdown('[Email](mailto:AlanFeder@gmail.com) | [Website](https://www.alanfeder.com/) | [LinkedIn](https://www.linkedin.com/in/alanfeder/) | [GitHub](https://github.com/AlanFeder)')
+
 
     with chat_container:
         if prompt1 := st.chat_input(placeholder=placeholder1, key='input1'):
